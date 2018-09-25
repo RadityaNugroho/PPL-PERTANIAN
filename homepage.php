2cc60312c25e9e3f1598fab67dee97d12b86089b
Khisyam <rkhisyam54@gmail.com>
2018-09-25 23:55:49 +0700

    sprint 1

---
+++ homepage.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME WABER</title>
+</head>
+<body>
+	<input type="button" value="Akun" name="akun">
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Warung Beras</title>
+  <link rel="stylesheet" type="text/css" href="css/mod.css">
+</head>
+<body>
+  <div id="atas">
+    <div id="nav-kiri">
+    <img src="img/padi.png" width="80px" height="40px">
+    <br>
+    Warung Beras
+    </div>
+    <table id="nav-akun">
+      <tr>
+        <td>
+          <div id="navkanan">
+          <button onclick="window.location.href='masuk.php'" type="login" >Masuk</button>
+          </div>
+        </td>
+        <td>
+          <div id="navkiri">
+          <button onclick="window.location.href='daftar.php'" type="daftar">Daftar</button>
+          </div>
+        </td>
+      </tr>
+    </table>
+    
+  </div>
+
+</body>
+</html>
